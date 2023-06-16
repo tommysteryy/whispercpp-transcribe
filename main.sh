185687e6690fa6017ff9966ecc4f788c7e408cab
Tommy Xu <tommysteryy@gmail.com>
2023-06-16 09:49:03 -0700

    made my own version of main

--- main.sh
+++ main.sh
@@ -10,9 +10,10 @@ file=""
 url=""
 mode=""
 lang="en"
+name=""
 
 # Parse the named arguments
-while getopts "f:u:m:l:s" opt; do
+while getopts "f:u:m:l:n:" opt; do
   case $opt in
     f) file="$OPTARG"
     ;;
@@ -22,16 +23,19 @@ while getopts "f:u:m:l:s" opt; do
     ;;
     l) lang="$OPTARG"
     ;;
-    s) summary=true
+    n) name="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
+echo "name is $name"
+echo "url is $url"
+
 # Check that either a file or a URL was specified
-if [ -z "$file" ] && [ -z "$url" ]; then
-  echo "Either a file or a URL must be specified"
+if [ -z "$url" ] && [ -z "$name" ]; then
+  echo "Either a Url or a Name must be specified"
   exit 1
 fi
 
@@ -42,6 +46,11 @@ else
   audio_file_name="temp"
 fi
 
+if [ ! -z "$name" ]; then
+  url="https://youtu.be/$name"
+  echo "Using $url as url"
+  echo "Using file path as daniellekirsty/$name"
+fi
 
 if [ ! -z "$url" ]; then
   echo "URL: $url"
@@ -52,19 +61,26 @@ fi
 msg "Converting mp3 to wav 16kHz..."
 ffmpeg -i "$audio_file_name.mp3" -ar 16000 -ac 1 -c:a pcm_s16le "$audio_file_name.wav" -nostats -loglevel 16 -y
 
-msg "Transcribing using model $2..."
-whisper.cpp/main -f "$audio_file_name.wav" -otxt -of transcript -nt -pp -m whisper.cpp/models/ggml-$model.bin -l $lang
+msg "Transcribing using model $model..."
+whisper.cpp/main -f "$audio_file_name.wav" -otxt -of "daniellekirsty/$name" -nt -pp -m whisper.cpp/models/ggml-$model.bin -l $lang
 
-msg "Formating output..."
-cmd="python3 format_and_summarize.py"
-if [ ! -z "$file" ]; then
-  duration=$(ffmpeg -i "$audio_file_name.mp3" 2>&1 | grep Duration | awk -F'[:,.]' '{print $2":"$3":"$4}')
-  cmd+=" -f $audio_file_name -d $duration"
-fi
-if [ "$summary" = true ]; then
-    cmd+=" -s"
-fi
-eval $cmd
+msg "All DONE!"
+end_time="$(date -u +%s)"
+secs="$(($end_time-$start_time))"
+elapsed=$(printf '%02dh:%02dm:%02ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)))
+msg "Total elapsed time: $elapsed"
+
+
+# msg "Formating output..."
+# cmd="python3 format_and_summarize.py"
+# if [ ! -z "$file" ]; then
+#   duration=$(ffmpeg -i "$audio_file_name.mp3" 2>&1 | grep Duration | awk -F'[:,.]' '{print $2":"$3":"$4}')
+#   cmd+=" -f $audio_file_name -d $duration"
+# fi
+# if [ "$summary" = true ]; then
+#     cmd+=" -s"
+# fi
+# eval $cmd
 
 
 msg "Clean up..."
@@ -72,10 +88,5 @@ rm -f temp.mp3
 rm -f $audio_file_name.wav
 rm -f temp.info.json
 rm -f temp.webm
-rm -f transcript.txt
+# rm -f transcript.txt
 
-msg "All DONE!"
-end_time="$(date -u +%s)"
-secs="$(($end_time-$start_time))"
-elapsed=$(printf '%02dh:%02dm:%02ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)))
-msg "Total elapsed time: $elapsed"
