8aeb9a6da1b0e7a42c29f8642ffee96a6da1fa45
Tommy Xu <tommysteryy@gmail.com>
2023-06-28 11:07:38 -0700

    modified to use new youtuber-finder

--- transcribe-gambier.sh
+++ transcribe-gambier.sh
@@ -2,15 +2,11 @@
 
 export PATH=$PATH:/home/t/txu25/.local/bin
 # Initialize a variable for the path and model
-path=""
 model=""
 
 # Use getopts to parse the command line options
-while getopts "p:m:" opt; do
+while getopts "m:" opt; do
   case $opt in
-    # If the option is p, set the path variable to its argument
-    p) path="$OPTARG"
-    ;;
     # If the option is m, set the model variable to its argument
     m) model="$OPTARG"
     ;;
@@ -20,20 +16,11 @@ while getopts "p:m:" opt; do
   esac
 done
 
-# Check if path was specified, if not, output an error message and exit with status 1
-if [ -z "$path" ]; then
-    echo "ERROR: Must specify a path for the transcripts to be downloaded into. See <repo_root>/transcripts for examples"
-    exit 1
-fi
-
 # If no model specified, default to "small.en"
 if [ -z "$model" ]; then
     echo "Did not specify model, so defaulting to small.en"
     model="small.en"
 fi
-
-# Change directory to our whisper cpp transcription service
-# cd whispercpp-transcribe
 
 # The file from which we will read the data
 inputFile="../curr_video_list_gambier.txt"
@@ -41,8 +28,19 @@ inputFile="../curr_video_list_gambier.txt"
 # This loop will continue as long as there are lines to be read from the file
 # We're using file descriptor 3 instead of stdin to avoid interference during subprocess calls.
 while IFS= read -r -u 3 line; do
+
+    # Find the path with the path finding script
+    youtuber=$(./find_creator.sh "$line")
+
+    # Check if the result starts with "Error" or "No file found", meaning it did not match a youtuber
+    
+    if [[ $youtuber == Error* || $youtuber == "No file found"* ]]; then
+        echo "Error finding path for $line: $youtuber. Skipping"
+        continue  # Skip to the next iteration
+    fi
+
     # Run the main.sh script with the current line, chosen model, and path as arguments
-    ./main-gambier.sh -n "$line" -m "$model" -p "$path"
+    ./main-gambier.sh -n "$line" -m "$model" -p "$youtuber"
 done 3<"$inputFile"
 
 # Exit the script
